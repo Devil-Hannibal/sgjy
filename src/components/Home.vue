b17600fe80f33cc2f554b58885d371e0f2153300
Devil-Hannibal <bfzy4444@163.com>
2019-04-26 17:37:29 +0800

    主页侧边菜单栏完成，less语法配置，编写轮播图组件中

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,243 +1,155 @@
 <template>
-  <div class="home">
-    <el-container>
-      <el-header>
-        <div class="card">
-          <!-- 登录注册区域 -->
-          <div class="nav">
-            <el-row>
-              <el-col :span="12">
-                <div class="fl_title">欢迎来到*-*******网！</div>
-              </el-col>
-              <el-col :span="12">
-                <div class="fr_title font16">
-                  <span>登录</span>
-                  <span @click="register">注册</span>
-                </div>
-              </el-col>
-            </el-row>
-          </div>
-          <!-- 搜索区域 -->
-          <div>
-            <el-row>
-              <!-- logo区域 -->
-              <el-col :span="4">
-                <div class="logo_l">
-                  <img src="./../assets/images/logo.png" alt>
-                </div>
-              </el-col>
-              <!-- 搜索框 -->
-              <el-col :span="14" class="paddingLeft138">
-                <div class="search">
-                  <input class="search_input" type="text">
-                  <button class="btn-search el-icon-search"></button>
-                  <!-- <img class="search" src="./../assets/images/search.png" alt> -->
-                </div>
-
-                <ul class="ul1">
-                  <li>
-                    <a href="javasrcipt:void(0)" class="ff5">热门搜索 :</a>
-                  </li>
-                  <li>
-                    <a href="javasrcipt:void(0)">考研政治</a>
-                  </li>
-                  <li>
-                    <a href="javasrcipt:void(0)">考研英语</a>
-                  </li>
-                  <li>
-                    <a href="javasrcipt:void(0)">考研数学</a>
-                  </li>
-                  <li>
-                    <a href="javasrcipt:void(0)">考研集训营</a>
-                  </li>
-                  <li>
-                    <a href="javasrcipt:void(0)">考研面试课</a>
-                  </li>
-                </ul>
-              </el-col>
-              <!-- 热线电话 -->
-              <el-col :span="6">
-                <div class="logo_r">
-                  <div class="tel">
-                    <img src="./../assets/images/phone.png" alt>
-                  </div>
-                  <div class="tel_r">
-                    <p>全国服务热线</p>
-                    <p class="phone_txt">15545452365</p>
-                  </div>
-                </div>
-              </el-col>
-            </el-row>
-          </div>
-          <!-- 菜单栏 -->
-
-          <div>
-            <div class="meus">
-              <span class="el-icon-s-fold"></span>
-              <span>行业分类</span>
-            </div>
-
-            <ul class="meus_ul">
-              <li>
-                <a href>关于我们</a>
-              </li>
-              <li>
-                <a href>培训课程</a>
-              </li>
-              <li>
-                <a href>培训学校</a>
-              </li>
-              <li>
-                <a href>讯息资讯</a>
-              </li>
-              <li>
-                <a href>教考知识</a>
-              </li>
-              <li>
-                <a href>明星推荐</a>
-              </li>
-              <li>
-                <a href>联系我们</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </el-header>
-      <el-main></el-main>
-      <el-footer>Footer</el-footer>
-    </el-container>
-  </div>
+	<div class="home">
+		<Header></Header>
+		<!-- 内容区域 -->
+		<div class="container">
+			<!-- 轮播图与菜单区域 -->
+			<div class="hyfl_box">
+				<!-- 行业分类	 -->
+				<div class="navMenus">
+					<ul>
+						<!-- 列表 -->
+						<li>
+							<p>
+								<span class="iconfont icon-shu"></span>
+								<span class="ts">学习辅导</span>
+							</p>
+							<p class="ts1"><span>拓展训练营</span>
+								<span> 中小学教育</span></p>
+							<span class="iconfont icon-jiantou iconPos"></span>
+						</li>
+						<li>
+							<p>
+								<span class="iconfont icon-shu"></span>
+								<span class="ts">学习辅导</span>
+							</p>
+							<p class="ts1"><span>拓展训练营</span>
+								<span> 中小学教育</span></p>
+							<span class="iconfont icon-jiantou iconPos"></span>
+						</li>
+						<li>
+							<p>
+								<span class="iconfont icon-shu"></span>
+								<span class="ts">学习辅导</span>
+							</p>
+							<p class="ts1"><span>拓展训练营</span>
+								<span> 中小学教育</span></p>
+							<span class="iconfont icon-jiantou iconPos"></span>
+						</li>
+						<li>
+							<p>
+								<span class="iconfont icon-shu"></span>
+								<span class="ts">学习辅导</span>
+							</p>
+							<p class="ts1"><span>拓展训练营</span>
+								<span> 中小学教育</span></p>
+							<span class="iconfont icon-jiantou iconPos"></span>
+						</li>
+						<li>
+							<p>
+								<span class="iconfont icon-shu"></span>
+								<span class="ts">学习辅导</span>
+							</p>
+							<p class="ts1"><span>拓展训练营</span>
+								<span> 中小学教育</span></p>
+							<span class="iconfont icon-jiantou iconPos"></span>
+						</li>
+						<li>
+							<p>
+								<span class="iconfont icon-shu"></span>
+								<span class="ts">学习辅导</span>
+							</p>
+							<p class="ts1"><span>拓展训练营</span>
+								<span> 中小学教育</span></p>
+							<span class="iconfont icon-jiantou iconPos"></span>
+						</li>
+						<li>
+							<p>
+								<span class="iconfont icon-shu"></span>
+								<span class="ts">学习辅导</span>
+							</p>
+							<p class="ts1"><span>拓展训练营</span>
+								<span> 中小学教育</span></p>
+							<span class="iconfont icon-jiantou iconPos"></span>
+						</li>
+					</ul>
+				</div>
+				<div class="lunbo">
+					<div class="banner_box">
+						<div class="banner_one">
+							<Banner></Banner>
+						</div>
+						<div class="newsList"></div>
+					</div>
+					<div class="banner_two"></div>
+				</div>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
-export default {
-  data() {
-    return {};
-  },
-  methods: {
-    register() {
-      console.log("111");
-      return;
-    }
-  }
-};
+	import Header from './Header';
+	import Banner from './Banner';
+
+	export default {
+
+		components: {
+			Header,
+			Banner
+		},
+		data() {
+			return {};
+		},
+		methods: {}
+	};
 </script>
 
-<style scoped>
-/* 整体容器样式 */
-.el-container {
-  width: 100%;
-}
-/* 头部区域样式后期可能抽离出去 */
-.el-header {
-  border-bottom: 2px solid #ff5722;
-  height: 215px !important;
-  padding: 0;
-}
-.el-header .card {
-  width: 1200px;
-  margin: 0 auto;
-}
-.nav {
-  height: 40px;
-  background-color: #f5f5f5;
-  line-height: 40px;
-}
-.fl_title {
-  color: #949494;
-  font-size: 12px;
-}
-.fr_title {
-  text-align: right;
-}
-/* 搜索logo区样式 */
-.search {
-  margin-top: 25px;
-  position: relative;
-}
-.search_input {
-  height: 47px;
-  width: 100%;
-  box-sizing: border-box;
-  border: 1px solid #d9d9d9;
-}
+<style lang="less" scoped="scoped">
+	.container {
+		width: 1200px;
+		margin: 0 auto;
+	}
 
-.btn-search {
-  width: 80px;
-  height: 47px;
-  position: absolute;
-  right: 0;
-  top: 0;
-  bottom: 0;
-  background-color: #ff5722;
-  border: none;
-}
-.el-icon-search {
-  color: white;
-  font-size: 20px;
-}
-.ul1 li {
-  float: left;
-  margin-top: 15px;
-  padding: 0px 13px;
-  font-size: 14px;
-}
-.ul1 li a {
-  color: #b2b2b2;
-}
-.ul1 li:last-child {
-  float: left;
-  padding: 0px 0px 0px 13px;
-}
-.paddingLeft138 {
-  padding-left: 138px;
-}
-.logo_r {
-  padding: 40px 0px 0px 100px;
-}
-.tel {
-  float: left;
-}
-.tel_r p:first-child {
-  text-align: right;
-}
-.tel_r p:last-child {
-  color: #ffa589;
-  font-size: 21px;
-  text-align: right;
-}
-.ff5 {
-  color: #ff5722;
-}
-/* 横菜单栏 */
-.meus {
-  width: 254px;
-  height: 38px;
-  line-height: 38px;
-  background-color: #ff5722;
-  color: white;
-  float: left;
-}
-.meus_ul {
-  /* padding-left: 52px;
-  padding-right: 52px; */
-  width: 946px;
-  height: 38px;
-  line-height: 38px;
-  float: left;
-}
+	.navMenus {
+		width: 252px;
+		border: 1px solid #ff5722;
+	}
+	.navMenus li {
+		position: relative;
+		padding-left: 20px;
+		height: 66px;
+		box-sizing: border-box;
+		border-bottom: 1px dashed #e7e7e7;
+	}
+	.navMenus li:last-child {
+		border-bottom: none;
+	}
+	.navMenus li p:first-child {
+		padding-top: 10px;
+	}
+	.icon-shu {
+		color: #ff5722;
+		font-size: 22px;
+	}
+	.iconPos {
+		position: absolute;
+		top: 50%;
+		right: 20px;
+		transform: translateY(-50%);
+		color: #999999;
+	}
+	.ts {
+		font-size: 18px;
+		color: #333333;
+		padding-left: 14px;
+	}
+	.ts1 {
+		color: #b4b4b4;
+		font-size: 15px;
 
-.meus_ul li {
-  float: left;
-  width: 14.28%;
-  text-align: center;
-}
-.meus_ul li a {
-  display: block;
-  width: 100%;
-  font-size: 16px;
-  color: #333333;
-}
+	}
+	.ts1 span:last-child {
+		padding-left: 10px;
+	}
 </style>
-
-

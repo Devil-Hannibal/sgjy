14daf0828a8c15525e9908f9ba4f86f705b56f36
Devil-Hannibal <bfzy4444@163.com>
2019-05-08 15:36:38 +0800

    文章详情  静态完成

--- src/components/nav/NewsDetails.vue
+++ src/components/nav/NewsDetails.vue
@@ -1,11 +1,56 @@
 <template>
-	<div>
-		sdasd dsa的撒
+	<div class="news_details">
+		<div>
+			<p>自考报名不考英语划算吗？</p>
+			<p>发表时间：2019-04-15 6人浏览</p>
+			<p> <img src="./../../../static/images/new_d1.png" alt=""></p>
+			<p>现在自考报名大家都知道有这样的一个规定，三门专业课可以取代英语考试，这表面上看起来是各位不喜欢英语的考生们的福音，那么自考报名选择三门公开课取代英语究竟划不划算呢?</p>
+			
+			<p>其实小编觉得只有一种情况是划算的就是针对于年龄较大的考试对英语0基础，本地化企业工作入职只差一个本科文凭或者升职只差一个文凭，且有着充足的时间备考的朋友们，这种情况多考几门其实无伤大雅。其余都是不怎么划算的</p>
+			<p>一、从拿证时间的角度分析</p>
+			<p class="textIndent2">大部分省自考一年为两次机会，每次顶多报4个科目，如果选专业选考外加必考科目的话你大概得考15门，选考英语(二)的话你只用考12门，需要一年半搞定自考，否则你再怎么快也得两年拿到证</p>
+			<p> <img src="./../../../static/images/new_d2.png" alt=""></p>
+			<p>二、从需要花费的时间分析</p>
+			<p class="textIndent2">
+				　　这个就不用讲太多了，大家心里都清楚3门是定比1门费时间的，你都能下狠心多考三门为啥不试试考英语呢，三倍的时间你都愿意花费了，而且大多考生们其实是有英语基础的，只要多背背单词多刷刷题，及格那也是分分钟的事情,另外三门可是完全陌生的哦</p>
+			<p>
+				三、从适用性来分析</p>
+			<p class="textIndent2">　　职场上碰到单词的可能性是相当高的，旅游去外面玩多会一门语言多一份生存能力，比较有些朋友自考的专业和自己的专业并不一样，单纯为了文凭的话那其实对你有用让你真正受益的反而是英语这门科目了</p>
+		</div>
 	</div>
 </template>
 
 <script>
 </script>
 
-<style>
+<style lang="less" scoped>
+	.news_details {
+		width: 100%;
+		padding: 0 20px 200px;
+		p{
+			line-height: 30px;
+			font-size: 14px;
+			color: #000;
+			margin-top:40px; 
+			&:nth-child(1){
+				font-size: 24px;
+				text-align: center;
+				background-color: #fff5f2;
+				height: 60px;
+				line-height: 60px;
+			}
+			&:nth-child(2){
+				font-size: 14px;
+				height: 15px;
+				color: #646464;
+				text-align: center;
+				margin-top: 25px;
+			}
+			&:nth-child(3){
+				margin-top: 135px;
+			}
+			
+		}
+	}
+
 </style>
